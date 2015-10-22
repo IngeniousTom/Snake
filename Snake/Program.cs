d1e92a739e3ffaad7b47a757082ca154afb8c8a8
Tommy91 <tommyfongenious@yandex.ru>
2015-10-22 21:53:35 +0500

    The game now is complete and ready for sale))))

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,28 +12,27 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 25);
-
+            
             //отрисовка рамочки
-            HorizontalLine topLine = new HorizontalLine(0, 78, 0, '+');        
-            HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '+');           
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');            
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');      
-            topLine.Draw();
-            bottomLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+                
             //отрисовка змейки
             Point p = new Point(3, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            //генератор еды
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
             food.Draw();
 
             while (true)
             {
+                if (walls.IsHit(snake) || snake.isHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
